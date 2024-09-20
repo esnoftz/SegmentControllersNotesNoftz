3ae2dbf796450fd6451b1dc779af876818db413b
Evangeline Noftz <esnoftz@gmail.com>
2024-09-20 07:55:00 -0500

    Initial Commit

---
+++ SegmentControllersNotesNoftzTests/SegmentControllersNotesNoftzTests.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentControllersNotesNoftzTests.swift
+//  SegmentControllersNotesNoftzTests
+//
+//  Created by EVANGELINE NOFTZ on 9/20/24.
+//
+
+import Testing
+@testable import SegmentControllersNotesNoftz
+
+struct SegmentControllersNotesNoftzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
