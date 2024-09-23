14d9a4238bac6f7b14b693e9fd7a51357203f617
esnoftz <esnoftz@gmail.com>
2024-09-22 22:10:22 -0500

    Finished notes.

--- SegmentControllersNotesNoftz/ViewController.swift
+++ SegmentControllersNotesNoftz/ViewController.swift
@@ -11,9 +11,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do notes here
+
     }
 
+    
+    @IBAction func segmentAction(_ sender: UISegmentedControl) {
+        
+        // returns the index of the choice that was selected
+        var select = sender.selectedSegmentIndex
+        
+        switch select {
+            case 0:
+                print("You selected pony!")
+            case 1:
+                print("You selected dog!")
+            case 2:
+                print("You selected bear!")
+            default:
+                print("You didn't select anything!")
+        }
+        
+    }
+    
+    
 
 }
 
